16b6a10685fc6d13cca88a7fa5840f3753da77aa
Manuel Solis <msolis@iMac-de-Manuel-2.local>
2022-11-21 23:52:58 -0600

    solved tab problem

--- assets/components/Home.js
+++ assets/components/Home.js
@@ -1129,85 +1129,14 @@ class Home extends Component {
                         </div>
                     </div>
                     </div>
+           
                     </Tab.Pane> },
                 { menuItem: 'Accordeon', render: () => 
                 <Tab.Pane> 
-
-                    <div class="ui styled fluid accordion">
-                    <div class="title active">
-                        <i class="dropdown icon"></i>
-                        What is a dog?
-                    </div>
-                    <div class="content active">
-                        <p class="transition visible" style="display: block !important;">A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
-                    </div>
-                    <div class="title">
-                        <i class="dropdown icon"></i>
-                        What kinds of dogs are there?
-                    </div>
-                    <div class="content">
-                        <p class="transition hidden">There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
-                    </div>
-                    <div class="title">
-                        <i class="dropdown icon"></i>
-                        How do you acquire a dog?
-                    </div>
-                    <div class="content">
-                        <p>Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
-                        <p>A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
-                    </div>
-                    </div>
-
                     <select class="ui fluid search dropdown" multiple="">
                         <option value="">State</option>
                         <option value="AL">Alabama</option>
                         <option value="AK">Alaska</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="CA">California</option>
-                        <option value="CO">Colorado</option>
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="DC">District Of Columbia</option>
-                        <option value="FL">Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="HI">Hawaii</option>
-                        <option value="ID">Idaho</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IN">Indiana</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NV">Nevada</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="NY">New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="OH">Ohio</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="OR">Oregon</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TN">Tennessee</option>
-                        <option value="TX">Texas</option>
-                        <option value="UT">Utah</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WA">Washington</option>
                         <option value="WV">West Virginia</option>
                         <option value="WI">Wisconsin</option>
                         <option value="WY">Wyoming</option>
@@ -1266,8 +1195,6 @@ class Home extends Component {
                             </div>
                         </div>
                         </div>
-
-
 
                         <h4 class="ui header">Inline</h4>
                         <p>A dropdown can be formatted to appear inline in other content</p>
